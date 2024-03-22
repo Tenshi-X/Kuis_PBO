2865155d0570d68549ca9164fdc4bf1153ab64cc
Tenshi-X <sevafarel17@gmail.com>
2024-03-22 21:25:57 +0700

    Add Project

---
+++ src/pkg123210061_kuis/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg123210061_kuis;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    //fungsi main awal untuk inisiasi
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new loginPage().setVisible(true);
+    }
+   
+}
